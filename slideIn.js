24a5ecb5bfb6a68af1aa7ec9edf9c5ce65f86353
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-08-28 00:18:58 +0200

    Update slideIn.js

--- slideIn.js
+++ slideIn.js
@@ -1,40 +1,67 @@
 /* eslint-disable semi */
 
-// sprawdzenie, jaka jest szerokość elementu (przy mniejszym ekranie) - dzięki temu można dynamicznie podmienić animację?
-// client width to jest dokładna szerokość po odjęciu marginesów, paddingu itp.
-// document.querySelector('.standardSection').clientWidth
+let containerWidth;
 
-
-
-const imgToSlideIn3 = document.querySelector('.toSlideIn3');
-let elementWidth;
-
-function outOfView () {
-  elementWidth = document.querySelector('.standardSection').clientWidth;
-//   imgToSlideIn3.style.left = elementWidth + 'px';
-  imgToSlideIn3.style.left = '100%';
+function checkContainerWidth () {
+  containerWidth = document.querySelector('.standardSection').clientWidth;
 }
 
-function slideInOnScroll () {
+// SECTION 3 - From Right to Left
+
+const imgToSlideIn3 = document.querySelector('.imgSlide3');
+
+function outOfViewRight () {
+  checkContainerWidth();
+  imgToSlideIn3.style.left = containerWidth + 'px';
+}
+
+function slideInOnScrollFromRight () {
   if (scrollY > 200) {
-    elementWidth = document.querySelector('.standardSection').clientWidth;
-    imgToSlideIn3.style.transform = 'translateX(-' + elementWidth + 'px)';
+    imgToSlideIn3.style.transform = 'translateX(-' + containerWidth + 'px)';
   }
-//   imgToSlideIn3.style.transform = 'translateX(-300px)';
 }
 
-// function slideInOnScroll () {
-//   if (scrollY > 1000 && window.innerWidth > 1059) {
-//     reviewsSection.classList.add('reviewsSlideInAnimation');
+// SECTION 4 - From Left to Right
+
+const imgToSlideIn4 = document.querySelector('.imgSlide4');
+
+function outOfViewLeft () {
+  checkContainerWidth();
+  imgToSlideIn4.style.right = containerWidth + 'px';
+}
+
+function slideInOnScrollFromLeft () {
+  if (scrollY > 500) {
+    imgToSlideIn4.style.transform = 'translateX(' + containerWidth + 'px)';
+  }
+}
+
+// SECTION 5 - From Right to Left
+
+const imgToSlideIn5 = document.querySelector('.imgSlide5');
+
+function outOfViewRight2 () {
+  checkContainerWidth();
+  imgToSlideIn5.style.left = containerWidth + 'px';
+}
+
+function slideInOnScrollFromRight2 () {
+  if (scrollY > 800) {
+    imgToSlideIn5.style.transform = 'translateX(-' + containerWidth / 1.5 + 'px)';
+  }
+}
+
+// function imageCenter () {
+//   if (window.innerWidth < 600) {
+//     imgToSlideIn5.style.transform = '';
+//     imgToSlideIn5.style.left = '40%';
 //   }
 // }
 
-// function removeSlideInClass () {
-//   if (window.innerWidth <= 1059 && reviewsSection.classList.contains('reviewsSlideInAnimation')) {
-//     reviewsSection.classList.remove('reviewsSlideInAnimation')
-//   }
-// }
-
-window.addEventListener('load', outOfView);
-window.addEventListener('resize', outOfView);
-window.addEventListener('scroll', slideInOnScroll);
+window.addEventListener('load', outOfViewRight);
+window.addEventListener('load', outOfViewRight2);
+window.addEventListener('load', outOfViewLeft);
+window.addEventListener('scroll', slideInOnScrollFromRight);
+window.addEventListener('scroll', slideInOnScrollFromRight2);
+window.addEventListener('scroll', slideInOnScrollFromLeft);
+// window.addEventListener('resize', imageCenter);
