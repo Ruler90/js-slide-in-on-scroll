586a0c266e5d49c89bd96694f1011d05358d5488
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-08-28 19:30:18 +0200

    Update slideIn.js

--- slideIn.js
+++ slideIn.js
@@ -1,43 +1,27 @@
 /* eslint-disable semi */
 
-let containerWidth;
-let containerHeight;
-
-function checkContainerWidth () {
-  containerWidth = document.querySelector('.standardSection').clientWidth;
-}
-function checkContainerHeight () {
-  containerHeight = document.querySelector('.standardSection').clientHeight;
-}
-
 // SECTION 3 - From Right to Left
 
 const imgToSlideIn3 = document.querySelector('.imgSlide3');
 
-function outOfViewRight () {
-  checkContainerWidth();
-  imgToSlideIn3.style.left = containerWidth + 'px';
-  imgToSlideIn3.style.bottom = '-4px';
-}
-
 function slideInOnScrollFromRight () {
   if (scrollY > 200) {
-    imgToSlideIn3.style.transform = 'translateX(-' + containerWidth + 'px)';
+    imgToSlideIn3.classList.remove('elementHidden');
+    imgToSlideIn3.classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
+    imgToSlideIn3.style.bottom = '-4px';
+    imgToSlideIn3.style.left = '0%';
   }
 }
 
 // SECTION 4 - From Left to Right
 
-const imgToSlideIn4 = document.querySelector('.textWithImage');
-
-function outOfViewLeft () {
-  checkContainerWidth();
-  imgToSlideIn4.style.right = containerWidth + 'px';
-}
+const elementToSlideIn4 = document.querySelector('.textWithImage');
 
 function slideInOnScrollFromLeft () {
   if (scrollY > 500) {
-    imgToSlideIn4.style.transform = 'translateX(' + containerWidth + 'px)';
+    elementToSlideIn4.classList.remove('elementHidden');
+    elementToSlideIn4.classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
+    elementToSlideIn4.style.right = '0%';
   }
 }
 
@@ -45,15 +29,11 @@ function slideInOnScrollFromLeft () {
 
 const imgToSlideIn5 = document.querySelector('.imgSlide5');
 
-function outOfViewBottom () {
-  checkContainerHeight();
-  imgToSlideIn5.style.top = containerHeight + 'px';
-  imgToSlideIn5.style.left = '40%';
-}
-
 function slideInOnScrollFromBottom () {
   if (scrollY > 800) {
-    imgToSlideIn5.style.transform = 'translateY(-' + containerHeight + 'px)';
+    imgToSlideIn5.classList.remove('elementHidden');
+    imgToSlideIn5.classList.add('animation', 'animation--time', 'animation__slideFromBottomToTop');
+    imgToSlideIn5.style.top = '0%';
   }
 }
 
@@ -61,28 +41,17 @@ function slideInOnScrollFromBottom () {
 
 const imgToSlideIn6 = document.querySelector('.imgSlide6');
 
-function outOfViewTop () {
-  checkContainerHeight();
-  imgToSlideIn6.style.bottom = containerHeight + 'px';
-  imgToSlideIn6.style.left = '25%';
-}
-
 function slideInOnScrollFromTop () {
   if (scrollY > 1100) {
-    imgToSlideIn6.style.transform = 'translateY(' + containerHeight + 'px)';
+    imgToSlideIn6.classList.remove('elementHidden');
+    imgToSlideIn6.classList.add('animation', 'animation--time', 'animation__slideFromTopToBottom');
+    imgToSlideIn6.style.bottom = '0%';
   }
 }
 
-window.addEventListener('load', function () {
-  outOfViewRight();
-  outOfViewLeft();
-  outOfViewBottom()
-  outOfViewTop()
-});
 window.addEventListener('scroll', function () {
   slideInOnScrollFromRight();
   slideInOnScrollFromLeft()
   slideInOnScrollFromBottom();
   slideInOnScrollFromTop();
 });
-// window.addEventListener('resize', imageCenter);
